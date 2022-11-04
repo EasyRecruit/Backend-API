b9a91ab17871d4f248bb21043123e82debb2a82a
arhinful <kofibusy@gmail.com>
2022-11-04 22:05:35 +0000

    wip:auth fix id

--- database/Models/BaseModel.py
+++ database/Models/BaseModel.py
@@ -1,9 +1,10 @@
 from tortoise.models import Model
 from tortoise import fields
+from typing import Optional
 
 
 class BaseModel(Model):
-    id: Option[int] = fields.IntField(pk=True, generated=True)
+    id: Optional[int] = fields.IntField(pk=True, generated=True)
     created_at = fields.DatetimeField(auto_now_add=True, null=True, generated=True)
     updated_at = fields.DatetimeField(auto_now=True, null=True, generated=True)
 
